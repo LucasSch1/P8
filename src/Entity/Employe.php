b68f75b32a9de10fbd82764f353f4e1d470fe212
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-02-18 14:22:56 +0100

    Création de l'entité Employe + son repository

---
+++ src/Entity/Employe.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmployeRepository::class)]
+class Employe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $emailPro = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $typeContrat = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateEntree = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $actif = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmailPro(): ?string
+    {
+        return $this->emailPro;
+    }
+
+    public function setEmailPro(string $emailPro): static
+    {
+        $this->emailPro = $emailPro;
+
+        return $this;
+    }
+
+    public function getTypeContrat(): ?string
+    {
+        return $this->typeContrat;
+    }
+
+    public function setTypeContrat(string $typeContrat): static
+    {
+        $this->typeContrat = $typeContrat;
+
+        return $this;
+    }
+
+    public function getDateEntree(): ?\DateTimeInterface
+    {
+        return $this->dateEntree;
+    }
+
+    public function setDateEntree(\DateTimeInterface $dateEntree): static
+    {
+        $this->dateEntree = $dateEntree;
+
+        return $this;
+    }
+
+    public function getActif(): ?int
+    {
+        return $this->actif;
+    }
+
+    public function setActif(int $actif): static
+    {
+        $this->actif = $actif;
+
+        return $this;
+    }
+}
