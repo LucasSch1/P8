719c9f54f4025c870881131a67395f578c936db2
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-02-24 15:16:05 +0100

    Modification du controller home pour y ajouter des routes pour accéder aux différents controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+
+
+    public function __construct(
+        private ProjetRepository $projetRepository,
+    ){}
+
+
+    #[Route('/projets', name: 'app_home')]
     public function index(): Response
     {
+        $projets = $this->projetRepository->findAll();
         return $this->render('index.html.twig', [
             'controller_name' => 'HomeController',
+            'projets' => $projets,
         ]);
     }
 
     #[Route('/employes', name: 'app_employes')]
-    public function employes(): Response
+    public function showEmployesPage(): Response
     {
         return $this->render('employes.html.twig', [
             'controller_name' => 'HomeController',
