7659d393a50909a0061949f971393c34c03eff7d
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-02-25 10:49:04 +0100

    Récupération des status et affichage dans la page d'un projet

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProjetRepository;
+use App\Repository\StatutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,15 +12,22 @@ final class ProjetController extends AbstractController
 {
     public function __construct(
         private ProjetRepository $projetRepository,
+        private StatutRepository $statutRepository,
     ){}
 
     #[Route('/projets/{id}/view', name: 'app_projet_view')]
     public function index(int $id): Response
     {
         $projet = $this->projetRepository->find($id);
+        $statuts = $this->statutRepository->findAll();
         return $this->render('projet/index.html.twig', [
             'controller_name' => 'ProjetController',
             'projet' => $projet,
+            'statuts' => $statuts,
         ]);
     }
+
+
+
+
 }
