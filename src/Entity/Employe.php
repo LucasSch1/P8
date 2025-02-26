5c19f132dc81cdafa274a54105e847cf5ee0e1ca
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-02-26 13:44:50 +0100

    Modification des relations et propriétés

--- src/Entity/Employe.php
+++ src/Entity/Employe.php
@@ -7,13 +7,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Projet;
 
 #[ORM\Entity(repositoryClass: EmployeRepository::class)]
 class Employe
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(name: 'employe_id', type: 'integer')]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
@@ -34,21 +35,8 @@ class Employe
     #[ORM\Column(type: Types::SMALLINT)]
     private ?int $actif = null;
 
-    /**
-     * @var Collection<int, Projet>
-     */
-    #[ORM\ManyToMany(targetEntity: Projet::class, mappedBy: 'employees')]
-    private Collection $projets;
 
-    public function __construct()
-    {
-        $this->projets = new ArrayCollection();
-    }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getNom(): ?string
     {
@@ -123,8 +111,28 @@ class Employe
     }
 
     /**
-     * @return Collection<int, Projet>
+     * @var Collection<int, Projet>
      */
+    #[ORM\ManyToMany(targetEntity: Projet::class, mappedBy: 'employes')]
+    private Collection $projets;
+
+
+    /**
+     * @var Collection<int, Tache>
+     */
+    #[ORM\OneToMany(targetEntity: Tache::class, mappedBy: 'employe', cascade: ['persist', 'remove'])]
+    private Collection $taches;
+    public function __construct()
+    {
+        $this->projets = new ArrayCollection();
+        $this->taches = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
     public function getProjets(): Collection
     {
         return $this->projets;
@@ -134,18 +142,43 @@ class Employe
     {
         if (!$this->projets->contains($projet)) {
             $this->projets->add($projet);
-            $projet->addEmployee($this);
+            $projet->addEmploye($this);
         }
-
         return $this;
     }
 
     public function removeProjet(Projet $projet): static
     {
         if ($this->projets->removeElement($projet)) {
-            $projet->removeEmployee($this);
+            $projet->removeEmploye($this);
         }
+        return $this;
+    }
 
+    /**
+     * @return Collection<int, Tache>
+     */
+    public function getTaches(): Collection
+    {
+        return $this->taches;
+    }
+
+    public function addTache(Tache $tache): static
+    {
+        if (!$this->taches->contains($tache)) {
+            $this->taches->add($tache);
+            $tache->setEmploye($this);
+        }
+        return $this;
+    }
+
+    public function removeTache(Tache $tache): static
+    {
+        if ($this->taches->removeElement($tache)) {
+            if ($tache->getEmploye() === $this) {
+                $tache->setEmploye(null);
+            }
+        }
         return $this;
     }
 }
