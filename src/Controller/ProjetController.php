2cd5804ffd2d060243f4754e5c757d64f567f644
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-02-27 14:48:14 +0100

    Ajout de la route pour le formulaire d'ajout de projet

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -3,17 +3,26 @@
 namespace App\Controller;
 
 
+use App\Entity\Projet;
+use App\Form\ProjetType;
+use App\Repository\EmployeRepository;
 use App\Repository\ProjetRepository;
 use App\Repository\StatutRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class ProjetController extends AbstractController
 {
     public function __construct(
         private ProjetRepository $projetRepository,
         private StatutRepository $statutRepository,
+        private EmployeRepository $employeRepository,
+        private EntityManagerInterface $entityManager,
+        private ValidatorInterface $validator,
     ){}
 
     #[Route('/projets/{id}/view', name: 'app_projet_view')]
@@ -30,6 +39,39 @@ final class ProjetController extends AbstractController
             'statuts' => $statuts,
             'employesProjet' => $employesProjet,
         ]);
+    }
+
+    #[Route('/projets/projet-add', name: 'app_projet_add')]
+    public function addProjet(Request $request): Response
+    {
+        $projet = new Projet();
+
+
+        $employes = $this->employeRepository->findAll();
+
+        $formAddProjet = $this->createForm(ProjetType::class, $projet,[
+            'employes' => $employes,
+        ]);
+        $formAddProjet->handleRequest($request);
+        if ($formAddProjet->isSubmitted()){
+            $errors = $this->validator->validate($formAddProjet);
+            if (count($errors) > 0) {
+                return new Response('Le formulaire n\'est pas valide', Response::HTTP_BAD_REQUEST);
+            }else
+            {
+                $this->entityManager->persist($projet);
+                $this->entityManager->flush();
+                return $this->redirectToRoute('app_projet_view', ['id' => $projet->getId()]);
+            }
+        }
+
+        return $this->render('projet/add.html.twig', [
+            'controller_name' => 'ProjetController',
+            'projet' => $projet,
+            'formAddProjet' => $formAddProjet->createView(),
+        ]);
+
+
     }
 
 
