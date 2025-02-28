6b3c576cfa97abd4183ab88e27c6d5211f232e79
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-02-28 15:01:07 +0100

    Ajout d'une fonction qui vérifie si le projet est archivé

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -9,6 +9,7 @@ use App\Repository\EmployeRepository;
 use App\Repository\ProjetRepository;
 use App\Repository\StatutRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,12 +26,27 @@ final class ProjetController extends AbstractController
         private ValidatorInterface $validator,
     ){}
 
+
+
+    private function checkIfProjetIsArchived(?Projet $projet): ?RedirectResponse
+    {
+        if(!$projet || $projet->isArchive()){
+            $this->addFlash('error', 'Ce projet est archivé et ne peut pas être consulté.');
+            return $this->redirectToRoute('app_home');
+        }
+        return null;
+    }
+
+
+
     #[Route('/projets/{id}/view', name: 'app_projet_view')]
     public function index(int $id): Response
     {
         $projet = $this->projetRepository->find($id);
         $statuts = $this->statutRepository->findAll();
-
+        if ($redirect = $this->checkIfProjetIsArchived($projet)) {
+            return $redirect;
+        }
         $employesProjet = $projet->getEmployes();
 
         return $this->render('projet/index.html.twig', [
@@ -76,7 +92,9 @@ final class ProjetController extends AbstractController
     public function editProjet(Request $request, int $id): Response
     {
         $projet = $this->projetRepository->find($id);
-
+        if ($redirect = $this->checkIfProjetIsArchived($projet)) {
+            return $redirect;
+        }
         $allEmployes = $this->employeRepository->findAll();
 
         $employesProjet = $projet->getEmployes();
@@ -104,6 +122,18 @@ final class ProjetController extends AbstractController
             'projet' => $projet,
             'formAddProjet' => $formAddProjet->createView(),
         ]);
+
+    }
+
+
+    #[Route('/projets/{id}/projet-delete', name: 'app_projet_delete')]
+    public function deleteProjet(Projet $projet, int $id, EntityManagerInterface $entityManager): Response
+    {
+        $projet = $this->projetRepository->find($id);
+
+        $this->entityManager->remove($projet);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('app_home');
 
     }
 
